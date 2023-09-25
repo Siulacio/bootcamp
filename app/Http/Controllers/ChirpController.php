63365e1f1de82cf02283967d4c5deb5ff2345ad8
Luis Felipe Palacio Salamanca <siulacio@hotmail.com>
2023-09-25 15:07:29 -0500

    feat: add Chirp model, migration and controller

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chirp;
+use Illuminate\Http\Request;
+
+class ChirpController extends Controller
+{
+    public function index()
+    {
+        // TODO
+    }
+
+    public function create()
+    {
+        // TODO
+    }
+
+    public function store(Request $request)
+    {
+        // TODO
+    }
+
+    public function show(Chirp $chirp)
+    {
+        // TODO
+    }
+
+    public function edit(Chirp $chirp)
+    {
+        // TODO
+    }
+
+    public function update(Request $request, Chirp $chirp)
+    {
+        // TODO
+    }
+
+    public function destroy(Chirp $chirp)
+    {
+        // TODO
+    }
+}
